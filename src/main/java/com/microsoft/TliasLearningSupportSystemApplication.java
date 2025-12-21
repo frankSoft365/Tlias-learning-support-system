f0278737afbe6e6989e0439196953c2b4a9b414a
frank <264430662@qq.com>
2025-12-21 19:20:47 +0800

    init

---
+++ src/main/java/com/microsoft/TliasLearningSupportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.microsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasLearningSupportSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasLearningSupportSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microsoft/TliasLearningSupportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TliasLearningSupportSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
