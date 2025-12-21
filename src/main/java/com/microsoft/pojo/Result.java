cfa7ff585471bed64d430e7050abec6fa8216d46
frank <264430662@qq.com>
2025-12-21 20:26:56 +0800

    准备工作

---
+++ src/main/java/com/microsoft/pojo/Result.java
@@ -0,0 +1,31 @@
+package com.microsoft.pojo;
+
+import lombok.Data;
+
+@Data
+public class Result {
+    // 状态码 1 成功 0 失败
+    private Integer code;
+    // 错误信息
+    private String msg;
+    // 数据
+    private Object data;
+
+    public static Result success() {
+        Result result = new Result();
+        result.code = 1;
+        result.msg = "success";
+        return result;
+    }
+    public static Result success(Object object) {
+        Result result = success();
+        result.data = object;
+        return result;
+    }
+    public static Result error(String msg) {
+        Result result = new Result();
+        result.code = 0;
+        result.msg = msg;
+        return result;
+    }
+}
---
+++ src/main/java/com/microsoft/service/impl/DeptServiceImpl.java
@@ -0,0 +1,7 @@
+package com.microsoft.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServiceImpl {
+}
